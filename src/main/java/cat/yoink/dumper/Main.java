b376d2a93bae6f645f13e027c8502392e98e96c7
Katatje <geertspruijt6@outlook.com>
2020-11-21 21:40:48 +0800

    Initial Commit

---
+++ src/main/java/cat/yoink/dumper/Main.java
@@ -0,0 +1,14 @@
+package cat.yoink.dumper;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "dumper")
+public class Main
+{
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+}
